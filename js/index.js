05e55644c92ecca737a2ff5b13ba7900c326c2d8
Jonathan Hanson <jhanson8@uoregon.edu>
2019-12-03 15:30:39 -0800

    updates one

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
+/* jshint esversion:6 */
+
 // Your code goes here
+
+
+//nav bar click
+document.querySelectorAll("a").forEach(a => {
+  a.addEventListener("click", function() {
+    a.style.color= "hotpink";
+  });
+});
+
+//header image
+const headerImage = document.querySelector("header > img");
+
+headerImage.addEventListener("mouseenter", () => {
+  headerImage.style.transform = "scale(1.2)";
+  headerImage.style.transition = "transform 0.3s";
+});
+
+//header image resize
+headerImage.addEventListener("mouseleave", () => {
+  headerImage.style.transform = "scale(1)";
+});
