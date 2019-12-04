05b1de6749fc1a18dbe2184d4f8bf23da9bbfd4d
Jonathan Hanson <jhanson8@uoregon.edu>
2019-12-04 01:22:14 -0800

    updates mvp

--- js/index.js
+++ js/index.js
@@ -22,3 +22,73 @@ headerImage.addEventListener("mouseenter", () => {
 headerImage.addEventListener("mouseleave", () => {
   headerImage.style.transform = "scale(1)";
 });
+
+
+//logo red
+const logoHeading = document.querySelector("h1");
+
+logoHeading.addEventListener("click", () => {
+  logoHeading.style.color = "red";
+});
+
+//logo click
+
+logoHeading.addEventListener("dblclick", () => {
+  alert("Welcome to Fun Bus!");
+});
+
+//change picture
+const newPic = document.querySelector(".img-fluid-rounded");
+
+window.addEventListener('resize', () => {
+  newPic.src = "img/berlin.jpg";
+});
+
+//new header resize
+window.addEventListener('resize', () => {
+  headerImage.src = "img/nightbus.jpg";
+});
+
+//ams
+const picMiddle = document.querySelector(".picMiddle");
+
+picMiddle.addEventListener('mousedown', () => {
+  picMiddle.src = "img/ams.jpg";
+});
+
+//night boat
+const boat = document.querySelector(".boat");
+
+boat.addEventListener('mouseover', () => {
+  boat.src = "img/nightboat.jpg";
+});
+
+//button
+const box = document.querySelector('.destination');
+box.addEventListener("click", () => {
+  box.style.backgroundColor = "blue";
+});
+
+
+//stopPropagation
+const button = document.querySelector(".btn");
+
+button.addEventListener('mouseup', (event)=>{
+  button.style.backgroundColor= "black";
+  console.log(event);
+  event.stopPropagation();
+});
+
+
+const buttonTwo = document.querySelector(".btn:nth-of-type(2)");
+
+buttonTwo.addEventListener('mouseup', ()=>{
+  buttonTwo.style.backgroundColor= "yellow";
+});
+
+//prevent default
+const stopNav = document.querySelector("a");
+stopNav.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("stopped the refresh");
+});
